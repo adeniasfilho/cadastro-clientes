d12459cea623d4664cbeda9d770ab19a4bea8ebf
adeniasfilho <adeniasfilho.n1@gmail.com>
2021-04-28 21:24:53 -0300

    Implantando método DELETE

--- src/index.js
+++ src/index.js
@@ -8,31 +8,47 @@ contador = 0;
 app.get("/clientes", (req, res) => {
     res.send(clientes);
 });
-app.put("/clientes", async(req, res) => { 
+app.put("/clientes", async (req, res) => {
     contador++;
     const {
-        cliente: [
-            {
-                nome:string,
-                endereco: string,
-                idade: int,
-                status: string 
-            }
-        ]
+        cliente: [{
+            nome,
+            endereco, 
+            idade,
+            status 
+        }]
+     
     } = req.body;
     clientes[contador] = {
         contador,
-        cliente
+        cliente : [{
+            nome,
+            endereco,
+            idade,
+            status
+       }]
     }
     await axios.post("http://localhost:10000/eventos", {
         tipo: "ClienteCriado",
         dados: {
             contador,
-            cliente,  
+            cliente : [{
+                nome,
+                endereco,
+                idade,
+                status
+            }]
         },
     });
     res.status(201).send(clientes[contador]);
 });
+app.delete("/clientes/:id", (req, res, next) => {
+    const idClienteDeletado = req.params.id;
+    clientes.forEach((cliente,index) => {
+        if(cliente.id == idClienteDeletado)clientes.splice(index);
+    })
+    res.status(201).json(clientes);
+})
 app.listen(4000, () => {
     console.log("clientes. Porta 4000");
 });
